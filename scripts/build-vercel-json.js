42606553596791f5f4960376b229b64652007341
Philipp Jardas <philipp@jardas.de>
2026-09-17 11:40:34 +0200

    chore: fix redirects, add vercel config file

---
+++ scripts/build-vercel-json.js
@@ -0,0 +1,43 @@
+import { readdir, readFile, writeFile } from "node:fs/promises";
+import { format } from "prettier";
+
+async function createRedirects() {
+  const talks = await readdir("talks");
+
+  const redirects = talks.map(async (talk) => {
+    const redirectsContent = await readFile(
+      `talks/${talk}/dist/_redirects`,
+      "utf-8",
+    );
+
+    const lines = redirectsContent
+      .split(/\n/g)
+      .map((s) => s.trim())
+      .filter(Boolean);
+
+    return lines.map((line) => {
+      const [source, destination] = line.split(/\s+/g);
+      return {
+        source: source.replace(/\*/g, "(.*)"),
+        destination,
+        permanent: false,
+      };
+    });
+  });
+
+  return (await Promise.all(redirects)).flat();
+}
+
+const vercelConfig = {
+  $schema: "https://openapi.vercel.sh/vercel.json",
+  installCommand: "pnpm install --frozen-lockfile",
+  buildCommand: "pnpm build && pnpm bundle",
+  outputDirectory: "dist",
+  redirects: await createRedirects(),
+};
+
+const json = await format(JSON.stringify(vercelConfig), {
+  filepath: "vercel.json",
+});
+
+await writeFile("vercel.json", json, "utf-8");
